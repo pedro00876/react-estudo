03c18a241d834cd5d740be4b50a862e8f0168a91
pedro00876 <pcchagas00@gmail.com>
2024-08-14 11:17:58 -0300

    Initial Commit

---
+++ 1_INTRO/react-project/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello Word React!</h1>
+      <h2>Rumo ao avançado!</h2>
+    </div>
+  );
+};
+
+export default App;
