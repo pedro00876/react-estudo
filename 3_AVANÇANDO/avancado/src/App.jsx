10247424cbac4c3b6aef2851520ac0a146c5d49c
pedro00876 <pcchagas00@gmail.com>
2024-08-16 11:47:43 -0300

    Avançado React

---
+++ 3_AVANÇANDO/avancado/src/App.jsx
@@ -0,0 +1,16 @@
+//Components
+import FirstComponent from "./Components/FirstComponent";
+
+///Style /Css
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Avançando em em React</h1>
+      <FirstComponent />
+    </div>
+  );
+};
+
+export default App;
