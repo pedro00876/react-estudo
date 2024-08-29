a6d37f7d7ac7cad4a49fd45198c24e4acb01eeaa
pedro00876 <pcchagas00@gmail.com>
2024-08-28 21:44:24 -0300

    Alternando as telas

--- 6_SECRET_WORD/secret-word/src/components/GameOver.jsx
+++ 6_SECRET_WORD/secret-word/src/components/GameOver.jsx
@@ -3,7 +3,7 @@ import "./GameOver.css";
 const GameOver = ({ retry }) => {
   return (
     <div>
-      <h1>Jogo Finalizado!</h1>
+      <h1>Game Over!</h1>
       <button onClick={retry}>Reiniciar o Jogo!</button>
     </div>
   );
