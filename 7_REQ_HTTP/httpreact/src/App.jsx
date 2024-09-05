b1db4c5758d3b98114ff0efacc1f8da61f88c9c6
pedro00876 <pcchagas00@gmail.com>
2024-09-05 12:13:22 -0300

    rescuing the api data

--- 7_REQ_HTTP/httpreact/src/App.jsx
+++ 7_REQ_HTTP/httpreact/src/App.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+const url = "http://localhost:3000/products";
+
 function App() {
-  return <h1>Lista de Produtos</h1>;
+  const [products, setProducts] = useState([]);
+
+  // 1-Resgatando dados
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((products) => (
+          <li key={products.id}>
+            {products.name} - R$: {products.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
