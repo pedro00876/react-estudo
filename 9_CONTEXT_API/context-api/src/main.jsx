28c760638964a9457263f3f8d49e38cba01d0361
pedro00876 <pcchagas00@gmail.com>
2025-05-14 11:02:59 -0300

    Criando provider

--- 9_CONTEXT_API/context-api/src/main.jsx
+++ 9_CONTEXT_API/context-api/src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { CounterContextProvider } from './context/CounterContext.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* 2 - criando provider */}
+    <CounterContextProvider>
+          <App />
+    </CounterContextProvider>
   </StrictMode>,
 )
