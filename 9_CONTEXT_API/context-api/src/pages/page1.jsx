5051e72e383103a3129ad8ba13cba85838e03bec
pedro00876 <pcchagas00@gmail.com>
2025-05-15 10:27:27 -0300

    Adicionanado page

--- 9_CONTEXT_API/context-api/src/pages/page1.jsx
+++ 9_CONTEXT_API/context-api/src/pages/page1.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const page1 = () => {
   return (
-    <div>page1</div>
+    <div> 
+      <h1>olá mundo</h1>
+      <p><link rel="stylesheet" href="style.css" /></p>
+    </div>
   )
 }
 
