8b2a317300d9cb111599e1340c38ea1db8c48c19
pedro00876 <pcchagas00@gmail.com>
2025-07-01 10:31:54 -0300

    Pages do projeto

--- 10_MINIBLOG/miniblog/src/App.jsx
+++ 10_MINIBLOG/miniblog/src/App.jsx
@@ -1,10 +1,32 @@
 import './App.css'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+
+
+// Pages
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+
+//Components
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+
 
 function App() {
 
   return (
     <div className='App'>
-      <h1>Miniblog</h1>
+      <BrowserRouter>
+      <Navbar />
+      
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/About' element={<About />} />
+          </Routes>
+        </div>
+        
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }
