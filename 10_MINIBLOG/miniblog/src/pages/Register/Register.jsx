5c579a2d0d6693146a8d93662e095ae02a6615cd
pedro00876 <pcchagas00@gmail.com>
2025-07-10 11:14:38 -0300

    Adicionando hook e registrando usuario no sistema

--- 10_MINIBLOG/miniblog/src/pages/Register/Register.jsx
+++ 10_MINIBLOG/miniblog/src/pages/Register/Register.jsx
@@ -1,4 +1,5 @@
 // CSS
+import { useAuthentication } from '../../hooks/useAuthentication'
 import styles from './Register.module.css'
 
 // Hooks
@@ -11,7 +12,9 @@ const Register = () => {
   const [confirmPassword, setConfirmPassword] = useState('')
   const [error, setError] = useState('')
 
-  const handleSubmit = (e) => {
+  const {createUser, error: authError, loading} = useAuthentication()
+
+  const handleSubmit = async (e) => {
     e.preventDefault()
 
     setError('')
@@ -27,9 +30,16 @@ const Register = () => {
       return
     }
 
-    console.log(user)
+    const res = await createUser(user)
+
+    console.log(res)
   }
 
+  useEffect(() => {
+    if(authError){
+      setError(authError);
+    }
+  }, [authError])
 
   return (
     <div className={styles.register}>
@@ -80,7 +90,8 @@ const Register = () => {
           onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </label>
-        <button className='btn'>Cadastrar</button>
+        {!loading && <button className='btn'>Cadastrar</button>}
+        {loading && <button className='btn' disabled>Aguarde...</button>}
         {error && <p className='error'>{error}</p>}
       </form>
     </div>
