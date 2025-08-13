75571931bdafc65a793166cc486d1b429062aac4
pedro00876 <pcchagas00@gmail.com>
2025-08-13 15:15:02 -0300

    Função de deletar

--- 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.jsx
+++ 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.jsx
@@ -6,21 +6,19 @@ import { Link } from 'react-router-dom'
 //hooks
 import { useAuthValue } from '../../context/AuthContext'
 import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+import { useDeleteDocument } from '../../hooks/useDeleteDocument'
 
 const Dashboard = () => {
   const { user } = useAuthValue()
   const uid = user.uid
 
-  
-  const  {documents: posts, loading } = useFetchDocuments("posts", null, uid)
-  const deleteDocument = (id) => {
+  const { documents: posts, loading } = useFetchDocuments('posts', null, uid)
 
-  }
+  const { deleteDocument } = useDeleteDocument('posts')
 
-  if(loading) {
+  if (loading) {
     return <p>Carregando...</p>
   }
-
 
   return (
     <div className={styles.dashboard}>
@@ -29,31 +27,42 @@ const Dashboard = () => {
       {posts && posts.length === 0 ? (
         <div className={styles.noposts}>
           <p>Não foram encontrados posts!</p>
-          <Link to='/posts/create' className="btn">
+          <Link to='/posts/create' className='btn'>
             Criar primeiro post
           </Link>
         </div>
       ) : (
         <>
-         <div className={styles.post_header}>
+          <div className={styles.post_header}>
             <span>Título</span>
             <span>Ações</span>
-         </div>
-
-         {posts && posts.map((posts) => (
-          <div key={posts.id} className={styles.post_row}>
-            <p>{posts.title}</p>
-            <div>
-              <Link to={`/posts/${posts.id}`} className='btn btn-outline'>Ver</Link>
-              <Link to={`/posts/edit${posts.id}`} className='btn btn-outline'>Editar</Link>
-              <button onClick={() => deleteDocument(posts.id)} className='btn btn-outline btn-danger'>Excluir</button>
-            </div>
           </div>
-        ))}
+
+          {posts &&
+            posts.map(posts => (
+              <div key={posts.id} className={styles.post_row}>
+                <p>{posts.title}</p>
+                <div>
+                  <Link to={`/posts/${posts.id}`} className='btn btn-outline'>
+                    Ver
+                  </Link>
+                  <Link
+                    to={`/posts/edit${posts.id}`}
+                    className='btn btn-outline'
+                  >
+                    Editar
+                  </Link>
+                  <button
+                    onClick={() => deleteDocument(posts.id)}
+                    className='btn btn-outline btn-danger'
+                  >
+                    Excluir
+                  </button>
+                </div>
+              </div>
+            ))}
         </>
       )}
-
-      
     </div>
   )
 }
