6ecca434162fb8cde67c199a08569b158de1e72d
pedro00876 <pcchagas00@gmail.com>
2025-09-12 20:49:05 -0300

    useRef

--- 11_HOOKS/react_hook/src/pages/Home.jsx
+++ 11_HOOKS/react_hook/src/pages/Home.jsx
@@ -1,22 +1,24 @@
 import HookUseEffect from '../components/HookUseEffect'
 import HookUseReducer from '../components/HookUseReducer'
 import HookUseState from '../components/HookUseState'
+import HookUseRef from '../components/HookUseRef'
 
 // useContext
 import { useContext } from 'react'
 import { SomeContext } from '../components/HookUseContext'
 
 const Home = () => {
-const {contextValue} = useContext(SomeContext)
+  const { contextValue } = useContext(SomeContext)
 
   return (
     <div>
       <HookUseState />
-      <HookUseReducer/>
+      <HookUseReducer />
       <HookUseEffect />
       <h2>useContext</h2>
       <p>Valor do context: {contextValue}</p>
       <hr />
+      <HookUseRef />
     </div>
   )
 }
